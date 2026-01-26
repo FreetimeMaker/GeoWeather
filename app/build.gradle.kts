575d963d04d273b09acd08a94435797ef82a6ef2
Freetime Maker <jamieachatz@gmail.com>
2026-01-26 22:52:34 +0100

    Changed a few things again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.freetime.geoweather"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.1.0"
+        versionCode = 12
+        versionName = "1.1.1"
     }
 
     buildFeatures {
