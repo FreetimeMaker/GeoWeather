128fa92f292467d1a9c3779f59c48222f13157b5
Freetime Maker <jamieachatz@gmail.com>
2026-02-06 21:09:40 +0100

    Update to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.freetime.geoweather"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.1.9"
+        versionCode = 21
+        versionName = "1.2.0"
     }
 
     buildFeatures {
