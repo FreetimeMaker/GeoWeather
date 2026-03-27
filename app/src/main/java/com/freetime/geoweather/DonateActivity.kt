5a1f3ea3d1749ff4f066c9a10319a66855c524e4
FreetimeMaker <jamie.achatz@s-stb.ch>
2026-03-27 11:10:45 +0100

    c again

--- app/src/main/java/com/freetime/geoweather/DonateActivity.kt
+++ app/src/main/java/com/freetime/geoweather/DonateActivity.kt
@@ -305,39 +305,6 @@ class DogeActivity : ComponentActivity() {
     }
 }
 
-class ShibActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        hideSystemUI()
-        setContent {
-            GeoWeatherTheme {
-                WebViewScreen(
-                    url = "https://ncwallet.net/pay/18spile",
-                    onBack = { finish() }
-                )
-            }
-        }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUI()
-        }
-    }
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN
-                )
-    }
-}
-
 class TronActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
