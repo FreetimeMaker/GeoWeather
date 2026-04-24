24965dbb1dbcc7e7a80b436a86f403fd5183974f
FreetimeMaker <jamie.achatz@s-stb.ch>
2026-04-24 09:18:53 +0000

    c

--- composeApp/src/wasmJsMain/kotlin/com/freetime/geoweather/Platform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/freetime/geoweather/Platform.wasmJs.kt
@@ -2,7 +2,11 @@ package com.freetime.geoweather
 
 import kotlinx.datetime.Instant
 
+external object Date {
+    fun now(): Double
+}
+
 @Suppress("NOTHING_TO_INLINE")
-internal inline fun nowMillis(): Long = (js("Date.now()") as Number).toLong()
+internal inline fun nowMillis(): Long = Date.now().toLong()
 
 actual fun currentInstant(): Instant = Instant.fromEpochMilliseconds(nowMillis())
