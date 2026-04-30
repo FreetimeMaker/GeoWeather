ea785cdf10d22a3ee32531239a6de6c3032a8adb
FreetimeMaker <jamie.achatz@s-stb.ch>
2026-04-30 12:10:02 +0000

    Updated so a Web Page will be deployed with build_and_co.yml and updated a few things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.freetime.geoweather"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.compileSdk.get().toInt()
-        versionCode = 39
-        versionName = "1.3.8"
+        versionCode = 40
+        versionName = "1.3.9"
     }
 
     buildFeatures {
