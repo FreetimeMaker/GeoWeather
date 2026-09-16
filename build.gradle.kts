7ec0869904990d9efd733c30e860c970c12e91a4
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-16 12:59:08 +0200

    Remove unused KMP Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,9 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.androidMultiplatformLibrary) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.composeMultiplatform) apply false
 }
