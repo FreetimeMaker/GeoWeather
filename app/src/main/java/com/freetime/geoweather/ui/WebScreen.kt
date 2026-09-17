7fb842b1987a65f27c0a7d3e143ad33719766932
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-17 11:51:18 +0200

    Remove shared R compatibility import

--- app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import com.freetime.geoweather.shared.R as Res
+import com.freetime.geoweather.R as Res
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
