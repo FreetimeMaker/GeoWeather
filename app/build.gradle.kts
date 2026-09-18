15ec07fafe9867978730fe2339f65795b2491ef0
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-18 20:18:23 +0200

    Test fletchmckee Liquid library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("io.github.kyant0:backdrop:2.0.0")
+    implementation("io.github.fletchmckee.liquid:liquid:1.1.1")
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.content.negotiation)
