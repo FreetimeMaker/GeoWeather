2edae90af8672de03b6a2ef5aacce0c7c09463f3
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-18 14:35:59 +0200

    Fix release Kotlin compilation errors

--- app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.freetime.geoweather.R as Res
 import com.freetime.geoweather.ui.glass.geoWeatherGlass
 
