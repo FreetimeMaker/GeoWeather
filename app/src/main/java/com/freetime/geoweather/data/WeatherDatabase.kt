ec4e0a4ceaad57985622a1fd8af7e7eaeabb0e09
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-19 18:02:01 +0200

    Migrate database for custom location ordering

--- app/src/main/java/com/freetime/geoweather/data/WeatherDatabase.kt
+++ app/src/main/java/com/freetime/geoweather/data/WeatherDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [LocationEntity::class, WeatherHistoryEntity::class], version = 7, exportSchema = false)
+@Database(entities = [LocationEntity::class, WeatherHistoryEntity::class], version = 8, exportSchema = false)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun locationDao(): LocationDao
     abstract fun weatherHistoryDao(): WeatherHistoryDao
@@ -20,5 +22,11 @@ fun getDatabaseBuilder(ctx: Context): RoomDatabase.Builder<WeatherDatabase> {
     )
 }
 
+private val MIGRATION_7_8 = object : Migration(7, 8) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE locations ADD COLUMN sortOrder INTEGER NOT NULL DEFAULT 0")
+    }
+}
+
 fun getRoomDatabase(builder: RoomDatabase.Builder<WeatherDatabase>): WeatherDatabase =
-    builder.fallbackToDestructiveMigration(true).build()
+    builder.addMigrations(MIGRATION_7_8).fallbackToDestructiveMigration(true).build()
