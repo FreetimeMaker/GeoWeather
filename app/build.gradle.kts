992e66e556d51ca79e1fc97ee207e9bbcccb6ec7
Jamie Achatz <jamieachatz@gmail.com>
2026-09-19 20:59:15 +0200

    Bump version to 4.2.0 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.freetime.geoweather"
         minSdk = 26
         targetSdk = 37
-        versionCode = 76
-        versionName = "4.1.0"
+        versionCode = 77
+        versionName = "4.2.0"
     }
 
     buildFeatures { compose = true }
