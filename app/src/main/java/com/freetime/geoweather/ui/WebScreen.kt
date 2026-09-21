2f9e157037b59031bdbe52c960173d3606988eea
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 12:44:50 +0200

    Fix direct Freetime Design imports

--- app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/WebScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.freetime.geoweather.R as Res
-import com.freetime.geoweather.ui.glass.freetimeGlass
-import com.freetime.geoweather.ui.glass.FreetimeGlassTopBar
+import me.free_time.design.freetimeGlass
+import me.free_time.design.FreetimeGlassTopBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
