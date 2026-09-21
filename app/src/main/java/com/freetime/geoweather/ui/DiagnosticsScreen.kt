009f0dbd5cfa0cbc3587451d1a1e1bf19fcdf954
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 12:45:09 +0200

    Fix direct Freetime Design imports

--- app/src/main/java/com/freetime/geoweather/ui/DiagnosticsScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/DiagnosticsScreen.kt
@@ -20,10 +20,10 @@ import androidx.compose.ui.unit.dp
 import com.freetime.geoweather.*
 import com.freetime.geoweather.data.DependencyManager
 import com.freetime.geoweather.R as Res
-import com.freetime.geoweather.ui.glass.freetimeGlass
-import com.freetime.geoweather.ui.glass.FreetimeGlassTopBar
-import com.freetime.geoweather.ui.glass.FreetimeGlassAction
-import com.freetime.geoweather.ui.glass.FreetimeGlassPanel
+import me.free_time.design.freetimeGlass
+import me.free_time.design.FreetimeGlassTopBar
+import me.free_time.design.FreetimeGlassAction
+import me.free_time.design.FreetimeGlassPanel
 import me.free_time.core.FreetimeCore
 
 @OptIn(ExperimentalMaterial3Api::class)
