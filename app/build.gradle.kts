13e31f1b93417db5c131dfde2fa85a1145ad1e63
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 20:39:41 +0200

    Remove direct Compose Material3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,6 @@ dependencies {
     implementation(composeBom)
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui-tooling-preview")
