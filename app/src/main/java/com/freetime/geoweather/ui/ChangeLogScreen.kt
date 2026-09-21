1fe4a9fa736636aca568e98534a3bbe61a982b64
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 12:45:23 +0200

    Fix direct Freetime Design imports

--- app/src/main/java/com/freetime/geoweather/ui/ChangeLogScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/ChangeLogScreen.kt
@@ -17,9 +17,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.freetime.geoweather.R as Res
-import com.freetime.geoweather.ui.glass.freetimeGlass
-import com.freetime.geoweather.ui.glass.FreetimeGlassTopBar
-import com.freetime.geoweather.ui.glass.FreetimeGlassPanel
+import me.free_time.design.freetimeGlass
+import me.free_time.design.FreetimeGlassTopBar
+import me.free_time.design.FreetimeGlassPanel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
