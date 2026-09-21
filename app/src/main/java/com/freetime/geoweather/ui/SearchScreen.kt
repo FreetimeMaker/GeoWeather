92cfe592bf139257f3028707c748517fe465d49b
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-21 12:45:12 +0200

    Fix direct Freetime Design imports

--- app/src/main/java/com/freetime/geoweather/ui/SearchScreen.kt
+++ app/src/main/java/com/freetime/geoweather/ui/SearchScreen.kt
@@ -21,9 +21,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.freetime.geoweather.R as Res
-import com.freetime.geoweather.ui.glass.freetimeGlass
-import com.freetime.geoweather.ui.glass.FreetimeGlassTopBar
-import com.freetime.geoweather.ui.glass.FreetimeTextField
+import me.free_time.design.freetimeGlass
+import me.free_time.design.FreetimeGlassTopBar
+import me.free_time.design.FreetimeTextField
 import kotlinx.coroutines.launch
 import me.free_time.design.FreetimeChip
 import me.free_time.design.FreetimeCard
