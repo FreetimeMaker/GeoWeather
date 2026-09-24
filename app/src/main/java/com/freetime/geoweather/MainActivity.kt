573212c3e51718f66b963183468a0238ba2e5d15
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-24 11:24:54 +0200

    Provide glass backdrop root for GeoWeather

--- app/src/main/java/com/freetime/geoweather/MainActivity.kt
+++ app/src/main/java/com/freetime/geoweather/MainActivity.kt
@@ -18,6 +18,7 @@ import com.freetime.geoweather.data.DependencyManager
 import com.freetime.geoweather.data.onCreateDocumentResult
 import com.freetime.geoweather.data.onOpenDocumentResult
 import com.freetime.geoweather.data.registerFilePickers
+import com.freetime.design.FreetimeGlassRoot
 
 class MainActivity : ComponentActivity() {
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { _ -> }
@@ -39,10 +40,12 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            WeatherApp(
-                database = DependencyManager.getDatabase(),
-                appSettings = DependencyManager.getAppSettings()
-            )
+            FreetimeGlassRoot {
+                WeatherApp(
+                    database = DependencyManager.getDatabase(),
+                    appSettings = DependencyManager.getAppSettings()
+                )
+            }
         }
     }
 
