8824978a469437a7f49f17dab86f4d88324c86ed
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-24 16:25:19 +0200

    Complete weather icon code mapping

--- app/src/main/java/com/freetime/geoweather/WeatherIconMapper.kt
+++ app/src/main/java/com/freetime/geoweather/WeatherIconMapper.kt
@@ -37,8 +37,13 @@ object WeatherIconMapper {
             3 -> Res.drawable.google_cloudy
             45, 48 -> Res.drawable.google_fog
             51, 53, 55 -> Res.drawable.google_drizzle
-            61, 63, 65 -> if (isDay) Res.drawable.google_rain_with_sunny_light else Res.drawable.google_rain_with_sunny_dark
-            71, 73, 75 -> if (isDay) Res.drawable.google_snow_with_sunny_light else Res.drawable.google_snow_with_sunny_dark
+            56, 57 -> Res.drawable.google_drizzle
+            61, 63, 65, 66, 67, 80, 81, 82 ->
+                if (isDay) Res.drawable.google_rain_with_sunny_light else Res.drawable.google_rain_with_sunny_dark
+            71, 73, 75, 77, 85, 86 ->
+                if (isDay) Res.drawable.google_snow_with_sunny_light else Res.drawable.google_snow_with_sunny_dark
+            95, 96, 99 ->
+                if (isDay) Res.drawable.google_rain_with_sunny_light else Res.drawable.google_rain_with_sunny_dark
             else -> if (isDay) Res.drawable.google_cloudy_with_sunny_light else Res.drawable.google_cloudy_with_sunny_dark
         }
     }
