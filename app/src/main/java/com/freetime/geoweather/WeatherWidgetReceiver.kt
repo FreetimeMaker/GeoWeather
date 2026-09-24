f59d9dcfd5cd188c8d040c4e6024ab33535ee1dc
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-24 14:28:54 +0200

    Register dedicated weather widget receivers

--- app/src/main/java/com/freetime/geoweather/WeatherWidgetReceiver.kt
+++ app/src/main/java/com/freetime/geoweather/WeatherWidgetReceiver.kt
@@ -6,3 +6,15 @@ import androidx.glance.appwidget.GlanceAppWidgetReceiver
 class WeatherWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = WeatherWidget()
 }
+
+class CompactWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = CompactWeatherWidget()
+}
+
+class ForecastWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = ForecastWeatherWidget()
+}
+
+class DetailedWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = DetailedWeatherWidget()
+}
