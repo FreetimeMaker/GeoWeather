e9e04cb7eea58d1d89e3473d0fe4a8fcb771698a
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-26 14:27:52 +0200

    Clean widget preferences on removal

--- app/src/main/java/com/freetime/geoweather/WeatherWidgetReceiver.kt
+++ app/src/main/java/com/freetime/geoweather/WeatherWidgetReceiver.kt
@@ -1,20 +1,28 @@
 package com.freetime.geoweather
 
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
-class WeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+abstract class BaseWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        appWidgetIds.forEach { WidgetLocationPreferences.delete(context, it) }
+        super.onDeleted(context, appWidgetIds)
+    }
+}
+
+class WeatherWidgetReceiver : BaseWeatherWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = WeatherWidget()
 }
 
-class CompactWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+class CompactWeatherWidgetReceiver : BaseWeatherWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = CompactWeatherWidget()
 }
 
-class ForecastWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+class ForecastWeatherWidgetReceiver : BaseWeatherWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = ForecastWeatherWidget()
 }
 
-class DetailedWeatherWidgetReceiver : GlanceAppWidgetReceiver() {
+class DetailedWeatherWidgetReceiver : BaseWeatherWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = DetailedWeatherWidget()
 }
